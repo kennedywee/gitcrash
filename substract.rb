2afe13fea2aead8252d3ea1fcde496bc66431085
Kennedy Wee <kennedy@elevateacademy.my>
2023-10-16 14:32:30 +0800

    Add substract

---
+++ substract.rb
@@ -0,0 +1,15 @@
+def substract(x, y)
+  x - y
+end
+
+puts "Substract number program by Kennedy Wee"
+
+puts "Enter first number: "
+input_x = gets.chomp.to_i
+
+puts "Enter second number: "
+input_y = gets.chomp.to_i
+
+result = substract(input_x, input_y)
+
+puts "This is the result: #{result}"
